2d051d90da7b62e13a17ce9d575780736364c9ca
PeterMotorniy <motorniy.peter@gmail.com>
2022-05-05 21:17:08 +0500

    commit to merge

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtCore import QRect, Qt
 
 from balls import Ball
-from game_model import GameModel
+from gameModel import GameModel
 from level import Level
 from userBall import userBall
 
@@ -22,9 +22,11 @@ class Game(QtWidgets.QFrame):
         self.main_window = main_window
 
         self.backMenu_btn = QtWidgets.QPushButton(self)
-        self.backMenu_btn.setGeometry(QtCore.QRect(25, 25, 40, 30))
+        self.backMenu_btn.setGeometry(QtCore.QRect(25, 25, 55, 45))
         self.backMenu_btn.setObjectName("backMenu_btn")
-        self.backMenu_btn.setText("Back")
+        self.backMenu_btn.setIcon(QtGui.QIcon('resources/arrow_white.png'))
+        self.backMenu_btn.setStyleSheet("background-color: rgba(255, 255, 255, 0);\n")
+        self.backMenu_btn.setIconSize(QtCore.QSize(55, 40))
         self.backMenu_btn.clicked.connect(self.back_menu_action)
 
         levels = list()
@@ -59,14 +61,14 @@ class Game(QtWidgets.QFrame):
             for i in self.model.level.balls:
                 draw_ball(painter, i)
 
-            draw_ball(painter, self.model.level.user_balls.static)
-            for i in self.model.level.user_balls.moving:
+            draw_ball(painter, self.model.level.userBallS.static)
+            for i in self.model.level.userBallS.moving:
                 draw_ball(painter, i)
 
             painter.end()
 
     def paintEvent(self, event):
-        self.model.update_game()
+        self.model.updateGame()
         self.model.collapse()
         self.draw()
         self.update()
